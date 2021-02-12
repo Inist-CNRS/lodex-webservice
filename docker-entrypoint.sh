042f03a068cd03a87aed7289ccbf34c6c87d95eb
Nicolas Thouvenin <nthouvenin@gmail.com>
2021-02-12 10:49:44 +0100

    chmod

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p /sbin/.npm /sbin/.config
 chown -R daemon:daemon /app /tmp /sbin/.npm /sbin/.config
+chmod 755 /app/public/expand.py
 npm run watch &
 exec su-exec daemon:daemon $*
